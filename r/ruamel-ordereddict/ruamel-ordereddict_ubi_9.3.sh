1eac61466b8c589ee7a2996d1f12582cea4512ed
Onkar Kubal <onkar82kubal@gmail.com>
2024-05-16 12:06:23 +0530

    changes

--- r/ruamel-ordereddict/ruamel-ordereddict_ubi_9.3.sh
+++ r/ruamel-ordereddict/ruamel-ordereddict_ubi_9.3.sh
@@ -33,21 +33,8 @@ cd Python-2.7.18
 cd /usr/local/bin
 ln -s python2.7 python
 cd $HOME_DIR
-
-curl -v -O https://files.pythonhosted.org/packages/b2/40/4e00501c204b457f10fe410da0c97537214b2265247bc9a5bc6edd55b9e4/setuptools-44.1.1.zip  -L
-unzip setuptools-44.1.1.zip
-cd setuptools-44.1.1
-python bootstrap.py
-
-mkdir wheels
-cd wheels
-curl -v -O https://files.pythonhosted.org/packages/53/7f/55721ad0501a9076dbc354cc8c63ffc2d6f1ef360f49ad0fbcce19d68538/pip-20.3.4.tar.gz  -L
-tar xzvf pip-20.3.4.tar.gz
-cd $HOME_DIR
-python ./wheels/pip-20.3.4-py2.py3-none-any.whl/pip install --no-index --find-links ./wheels/ pip --ignore-installed
-
-# python ./setuptools-44.1.1/setup.py install
-
+curl -v -O https://bootstrap.pypa.io/pip/2.7/get-pip.py -L
+python -m ensurepip --default-pip
 
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
@@ -56,7 +43,7 @@ if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     	exit 0
 fi
 cd $PACKAGE_NAME
-# pip install setuptools==40.0.0
+# pip install setuptools==19.2.3
 git checkout $PACKAGE_VERSION
 if ! python setup.py install; then
     echo "------------------$PACKAGE_NAME:install_fails---------------------------------------"
@@ -75,52 +62,3 @@ else
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE  | Pass |  Both_Install_and_Test_Success"
 	exit 0
 fi
-
-# yum -y update && yum install -y git gcc gcc-c++ make redhat-rpm-config 
-# # python-devel
-# curl -v -O https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz -L
-# tar xzvf Python-2.7.18.tgz
-# cd Python-2.7.18
-# ./configure && make && make altinstall
-# cd /usr/local/bin
-# ln -s python2.7 python
-# cd $HOME_DIR
-# yum install pip -y
-# # pip install virtualenv
-# which python
-
-# OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
-# echo $OS_NAME
-
-# if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-#     	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-# 		echo "$PACKAGE_URL $PACKAGE_NAME"
-# 		echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail |  Clone_Fails"
-#     	exit 0
-# fi
-
-# cd $PACKAGE_NAME
-# # virtualenv -p /usr/local/bin/python isoEnv
-# # pip install setuptools==44.0.0
-# pip install setuptools==2.0
-# pip install mercurial==5.2
-# git checkout $PACKAGE_VERSION
-# if ! python setup.py install; then
-#     echo "------------------$PACKAGE_NAME:install_fails---------------------------------------"
-# 	echo "$PACKAGE_URL $PACKAGE_NAME"
-# 	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail |  Install_Fails"
-# 	exit 0
-# fi
-
-# cd $PACKAGE_NAME
-# if ! python test/testordereddict.py; then
-# 	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-# 	echo  "$PACKAGE_URL $PACKAGE_NAME " 
-# 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail |  Install_success_but_test_Fails"
-# 	exit 0
-# else
-# 	echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
-# 	echo  "$PACKAGE_URL $PACKAGE_NAME " 
-# 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE  | Pass |  Both_Install_and_Test_Success"
-# 	exit 0
-# fi
